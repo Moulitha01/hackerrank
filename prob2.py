7cc0aac1d471fb60ea31857848c23dc4a12338c4
Moulitha01 <moulithac@gmail.com>
2026-04-17 22:25:26 +0530

    Update prob2.py

--- prob2.py
+++ prob2.py
@@ -1,7 +1,7 @@
 """Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
 
 Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function."""
-def is_leap(year):
+def isleap(year):
     if year % 400 == 0:
         return True
     elif year % 100 == 0:
@@ -12,7 +12,7 @@ def is_leap(year):
         return False
 
 year = int(input())
-print(is_leap(year))
+print(isleap(year))
 
 year = int(input())
-print(is_leap(year))
+print(isleap(year))
